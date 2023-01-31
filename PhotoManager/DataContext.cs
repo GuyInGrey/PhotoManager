f0b5838aa9939622b2ceba3a1f00eace7728611e
GuyInGrey <grey@guyingrey.com>
2023-01-30 22:12:16 -0700

    A

---
+++ PhotoManager/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using PhotoManager.Models;
+
+namespace PhotoManager
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Photo> Photo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) { return; }
+            optionsBuilder.UseSqlServer("Server=ARTANIS;Database=PhotoManager;Trusted_Connection=True;", options => { });
+        }
+    }
+}
---
+++ PhotoManager/Models/Photo.cs
@@ -0,0 +1,13 @@
+﻿namespace PhotoManager.Models
+{
+    public class Photo
+    {
+        public int PhotoId { get; set; }
+        public DateTimeOffset AddTime { get; set; }
+        public string Path { get; set; }
+        public string Hash { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public DateTimeOffset TakenTime { get; set; }
+    }
+}
